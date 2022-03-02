9ff18b62cbe1be885eee4ddb70ed7725ce1a3542
Sumit Vekariya <sumitvekariya7@gmail.com>
2022-03-02 13:04:48 +0530

    contract addresses updated

--- config.js
+++ config.js
@@ -2,37 +2,41 @@ require('dotenv').config()
 
 module.exports = {
   deployments: {
-    netId1: {
+    netId4: {
       eth: {
         instanceAddress: {
-          '0.1': undefined,
-          '1': undefined,
-          '10': undefined,
-          '100': undefined
+          0.1: "0x8e1657ffdf84Ef90c25E8dc465Fa23127f4185Ef",
+          1: "0xf88BD8230A9e3Ad6cEE4A14B5B5D834084E80371",
+          10: "0x0415F93a1b846EB4471681680f948b34BcEb9156",
+          100: "0xC18b26cC6C19BcEBbfdC456029eE93181e933BfD"
         },
         symbol: 'ETH',
         decimals: 18
-      },
+      }
     },
     netId42: {
       eth: {
         instanceAddress: {
-          '0.1': undefined,
-          '1': undefined,
-          '10': undefined,
-          '100': undefined
+          0.1: "0x8e1657ffdf84Ef90c25E8dc465Fa23127f4185Ef",
+          1: "0xf88BD8230A9e3Ad6cEE4A14B5B5D834084E80371",
+          10: "0x0415F93a1b846EB4471681680f948b34BcEb9156",
+          100: "0xC18b26cC6C19BcEBbfdC456029eE93181e933BfD"
         },
         symbol: 'ETH',
         decimals: 18
-      },
-      ceth: {
+      }
+    },
+    netId80001: {
+      eth: {
         instanceAddress: {
-          '1': '0x6504342B28d20853f87BC3E2e63012c30AB8080C',
+          0.1: "0xaAa355c04bee5D0dAd9f774F30c0Fb49FEdF252e",
+          1: "0x147A4B5a098d71E457030E2F0631a00e62680102",
+          10: "0x13742E4Ed90B6ff8B73A763670ae6FAbb250767c",
+          100: "0x6944D64CC1487a2715EE35aef617f8767DF0815e"
         },
-        tokenAddress: '0x41b5844f4680a8c38fbb695b7f9cfd1f64474a72',
-        symbol: 'cETH',
-        decimals: 8
-      },
+        symbol: 'ETH',
+        decimals: 18
+      }
     }
   }
 }
