9a82e10467c74091c86415965c3a0ce5f809c330
softskillpro <softskill.pro20@gmail.com>
2022-07-29 09:23:38 +0000

    Upgrade sol version

--- contracts/Mocks/ERC20Mock.sol
+++ contracts/Mocks/ERC20Mock.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-  constructor() ERC20("DAIMock", "DAIM") public {
+  constructor() ERC20("DAIMock", "DAIM") {
   }
 }
